56dca1e5eaec3e02234dbf7ec592ee234fe1ceb0
jswizzy <smithjustinn@hotmail.com>
2017-05-27 23:44:14 -0400

    add OOP

--- src/main.go
+++ src/main.go
@@ -2,60 +2,103 @@ package main
 
 import (
 	"fmt"
-	"os"
-)
-
-var (
-	plantCapacities = []float64{30, 30, 30, 60, 60, 100}
-	activePlants    = []int{0, 1}
-	gridLoad        = 75.0
+	"strings"
+	"errors"
 )
 
 func main() {
-
-	for {
-		message()
-
-		menu()
+	plants := []PowerPlant{
+		{hydro, 300, active},
+		{wind,30, active},
+		{wind, 25, inactive},
+		{wind, 35, active},
+		{solar, 45, unavailable},
+		{solar, 40, inactive},
 	}
 
+	grid := PowerGrid{300, plants}
+
+
+
+	if option, err := requestOption(); err == nil {
+		fmt.Println("")
+
+		switch option {
+		case "1":
+			grid.generatePlantReport()
+		case "2":
+			grid.generateGridReport()
+		}
+	} else {
+		fmt.Println(err.Error())
+	}
 }
 
-func message() {
-	fmt.Println()
+func requestOption() (option string, err error)  {
 	fmt.Println("1) Generate Power Plant Report")
 	fmt.Println("2) Generate Power Grid Report")
-	fmt.Println("Please choose an option: ")
-}
+	fmt.Print("Please choose an option: ")
 
-func menu() {
-	var option string
 	fmt.Scanln(&option)
 
-	switch option {
-	case "1":
-		generatePlantCapacityReport(plantCapacities...)
-	case "2":
-		generatePowerGridReport(activePlants, plantCapacities, gridLoad)
-	default:
-		fmt.Println("Unknown Command: exiting")
-		os.Exit(0)
+	if option != "1" && option != "2" {
+		err = errors.New("Invalid option selected")
+	}
+
+	return
+}
+
+type PlantType string
+
+const (
+	hydro PlantType = "Hydro"
+	wind  PlantType = "Wind"
+	solar PlantType = "Solar"
+)
+
+type PlantStatus string
+
+const (
+	active PlantStatus = "Active"
+	inactive PlantStatus = "Inactive"
+	unavailable PlantStatus = "Unavailable"
+)
+
+type PowerPlant struct {
+	plantType PlantType
+	capacity float64
+	status PlantStatus
+}
+
+type PowerGrid struct {
+	load float64
+	plants []PowerPlant
+}
+
+func (pg *PowerGrid) generatePlantReport()  {
+	for idx, p := range pg.plants {
+		label := fmt.Sprintf("%s%d", "Plant #", idx)
+		fmt.Println(label)
+		fmt.Println(strings.Repeat("-", len(label)))
+		fmt.Printf("%-20s%s\n", "Type:", p.plantType)
+		fmt.Printf("%-20s%.0f\n", "Capacity:", p.capacity)
+		fmt.Printf("%-20s%s\n", "Status:", p.status)
+		fmt.Println("")
 	}
 }
 
-func generatePlantCapacityReport(plantCapacities ...float64) {
-	for idx, cap := range plantCapacities {
-		fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
-	}
-}
-
-func generatePowerGridReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
-	capacity := 0.0
-	for _, plantId := range activePlants {
-		capacity += plantCapacities[plantId]
+func (pg *PowerGrid) generateGridReport() {
+	capacity := 0.
+	for _, plant := range pg.plants {
+		if plant.status == active {
+			capacity += plant.capacity
+		}
 	}
 
-	fmt.Printf("%-20s%.0f\n", "Capacity ", capacity)
-	fmt.Printf("%-20s%.0f\n", "Load ", gridLoad)
-	fmt.Printf("%-20s%.0f\n", "Utilization: ", gridLoad/capacity*100)
+	label := "Power Grid Report"
+	fmt.Println(label)
+	fmt.Println(strings.Repeat("-", len(label)))
+	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load:", pg.load)
+	fmt.Printf("%-20s%.2f%%\n", "Utilization:", pg.load/capacity*100)
 }
